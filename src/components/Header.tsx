4e9391cfc636fc1b3b818ee4fcf802406fe916b7
FABRICIO LOPES <92175971+XFBC@users.noreply.github.com>
2023-02-15 15:33:52 -0300

    remove typewriter react

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,21 @@
 import React from 'react'
 import avatar from './../../public/assets/hero-image.png'
 import SwiperStacks from './SwiperStacks'
-import TypeWriterEffect from 'react-typewriter-effect'
 
 const Header = () => {
   return (
     <section className="">
       <div className="grid md:grid-cols-2 grid-cols-1">
-        <div className="md:pt-[150px] pt-14">
+        <div className="pt-[150px]">
           <h1 className="bg-gradientText bg-clip-text text-transparent text-[62px] font-bold">
             Olá, <br /> sou Fabricio Lopes
           </h1>{' '}
-          <span className="text-[40px] text-[#81AFDD]"></span>
-          <TypeWriterEffect
-            textStyle={{
-              color: '#f49867',
-              fontWeight: 500,
-              fontSize: '30px'
-            }}
-            startDelay={1000}
-            cursorColor="#3F3D56"
-            multiText={['Desenvolvedor Front-end']}
-            multiTextDelay={1000}
-            typeSpeed={100}
-          />
+          <span className="text-[40px] text-[#81AFDD]">
+            Desenvolvedor Front-end
+          </span>
         </div>
         <div>
-          <img src={avatar} alt="" className="w-[600px] " />
+          <img src={avatar} alt="" className="w-[600px]" />
         </div>
       </div>
 
