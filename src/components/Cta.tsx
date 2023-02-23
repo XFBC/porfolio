f260895bb037c564f4a40d0ac2387eb63a2296aa
FABRICIO LOPES <92175971+XFBC@users.noreply.github.com>
2023-02-23 20:06:52 -0300

    add curriculo

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -11,7 +11,7 @@ const Cta = () => {
           contribuir para o sucesso de sua empresa.
         </h2>
         <a
-          href=""
+          href="/assets/curriculo.pdf"
           target="_blank"
           className="bg-black text-[#FFFF] px-[40px] py-[14px] rounded-[40px] md:mt-0 mt-4 flex flex-row items-center gap-2 hover:bg-gray-900"
         >
