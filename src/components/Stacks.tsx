8e6b6d7cad30ba4ab7f30313b88a097c3a36a919
FABRICIO LOPES <92175971+XFBC@users.noreply.github.com>
2023-03-30 20:00:59 -0300

    feat/ add prismic cms stacks

--- src/components/Stacks.tsx
+++ src/components/Stacks.tsx
@@ -1,18 +1,28 @@
-
-import { stacks } from '../constants/mock'
-const Stacks = () => {
+interface stackProps {
+  image: string
+}
+const Stacks = ({ image }: stackProps) => {
   return (
     <div>
       <div className="flex justify-center items-center flex-row">
-        {stacks.map(stack => (
+        {/* {stacks.map(stack => (
           <div className='relative xs:w-[210px] w-[170px] xs:mx-10 mx-5 xs:my-5 my-2  cursor-pointer flex justify-center '>
             <img
               src={stack.image}
-              alt=""
+              alt="logo"
               className="md:w-24 w-16 grayscale-[1] hover:grayscale-0  transition delay-100"
             />
           </div>
-        ))}
+        ))} */}
+
+        <div className="relative xs:w-[210px] w-[170px] xs:mx-10 mx-5 xs:my-5 my-2  cursor-pointer flex justify-center ">
+          <img
+            // src="/assets/stack-1.svg"
+            src={image}
+            alt="logo"
+            className="md:w-24 w-16 grayscale-[1] hover:grayscale-0  transition delay-100"
+          />
+        </div>
       </div>
     </div>
   )
--- src/components/SwiperStacks.tsx
+++ src/components/SwiperStacks.tsx
@@ -1,14 +1,17 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/pagination'
 import Stacks from './Stacks'
 import Marquee from 'react-fast-marquee'
+import { useAllPrismicDocumentsByType } from '@prismicio/react'
 
 export default function App() {
+  const [prismicStacks] = useAllPrismicDocumentsByType<any>('stacks')
+  console.log(prismicStacks)
   return (
     <div className="md:mt-[70px] mt-8">
       <Marquee gradient={false} speed={80}>
-        <Stacks />
+        {prismicStacks?.map((item) => (
+          <Stacks image={item.data.image.url}  />
+        ))}
+        
       </Marquee>
     </div>
   )
